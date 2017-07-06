6b03e5857129f6a43719fd1fc0aff896c9c4d2c0
Peter Xeros <pxeros@purdue.edu>
2017-07-06 18:35:44 -0400

    Modifications to functions.rs
    Changes: 
    A basic(ish) form of a reverse polish notation calculator has now been created. This took a long time. 
    There are more things to be implemented as time goes on: 
    I do plan on implementing a function that will convert reverse polish notation to infix expression, and with that I may be able to expand upon my simple arithmetic function to solve infix expressions that need an order of operations (I'll probably be using the Shunting-yard algorithm for that):
    As you can see in the code: I have the rpn to infix commented out, and I have also attempted to create an external function to reverse a vector and return it. This is where I ran into my issues with the lifetime of variables.
    The exact error I was getting is posted to the master branch as a PNG file so you can view it.   
    I still need to learn more on this lifetime of variables thing, but the compiler errors do not make it clear, I eventually just gave up and moved all the stuff for reversing the vector into the

--- functions.rs
+++ functions.rs
@@ -18,24 +18,103 @@ pub fn simple_arithmetic() {
 	}
 	let num1 = eq_vector[0].trim().parse::<i32>().unwrap();
 	let num2 = eq_vector[2].trim().parse::<i32>().unwrap();
-	let mut num3 = 0;
-	if eq_vector[1]=="+" {
-		num3 = num1 + num2;
-	}else if eq_vector[1]=="*" {
-		num3 = num1 * num2;
-	}else if eq_vector[1]=="-" {
-		num3 = num1 - num2;
-	}else if eq_vector[1]=="/" {
-		num3 = num1/num2;
-	}else if eq_vector[1]=="%" {
-		num3 = num1%num2;
+	let num3 = do_op(num1, num2, eq_vector[1]);
+	println!("{one} {two} {three} = {four}", one = num1, two = eq_vector[1], three = num2, four = num3);
+}
+fn do_op(num1: i32, mut num2: i32, oper: &str) -> i32 {
+	let mut result = 0;
+	let op = oper.trim();
+	if op=="+" {
+		result = num1 + num2;
+	}else if op=="*" {
+		result = num1 * num2;
+	}else if op=="-" {
+		result = num1 - num2;
+	}else if op=="/" {
+		result = num1/num2;
+	}else if op=="%" {
+		result = num1%num2;
+	}else if op=="^" {
+		result = 1;
+		while num2>0 {
+			result = result*num1;
+			num2-=1;
+		}
 	}else{
 		println!("Not a valid operation");
 	}
-	println!("{one} {two} {three} = {four}", one = num1, two = eq_vector[1], three = num2, four = num3);
+	result
 }
+fn get_precedence(op: &str) -> i32 {
+	if op.trim()=="^" {
+		4
+	}else if op.trim()=="*" || op.trim()=="/" || op.trim()=="%" {
+		3
+	}else if op.trim()=="+" || op.trim()=="-" {
+		2
+	}else {
+		return -1;
+	}
+}
+//This function will be coming soon: It will print the postfix expression into infix
+/*fn rpntoinfix(rpn: Vec<&str>) -> String {
+	let mut alg_stack: Vec<&str> = vec![];
+	let mut alg_string = String::from("");
+	let mut precedence: Vec<i32> = vec![];
+	for i in 0..rpn.len() {
+		let num = rpn.peek().parse::<i32>().unwrap();
+		match num {
+			Ok(num) {alg_stack.push(rpn.pop()); 
+					 continue;
+					}
+		}
+		precedence.push(get_precedence(rpn.peek()));
+		let val1 = alg_stack.pop();
+		let val2 = alg_stack.pop();
+		let short = String::from("");
+		short.push(String::from(val1.trim()));
+		short.push(String::from(rpn.pop().trim()));
+		short.push(String::from(val2.trim()));
+		alg_stack.push(&short);
+	}
+}*/
+/*fn reverse_stack<'a>(normal: String) -> Vec<&'a str> {
+	let mut norm: Vec<&str> = normal.split(" ").collect();
+	let mut new: Vec<&str> = vec![];
+	for i in 0..norm.len() {
+		new.push(norm.pop().unwrap());
+	}
+	new
+}*/
 pub fn reverse_polish_notation(){
-	println!("This is the rpn calculator, it is not finished yet");
+	println!("Please enter your polish notation string:\nPlease insert a space (\" \") between each item in your input");
+	let mut rpn = get_input();
+	rpn.pop();
+	//reversing the stack
+	let mut norm: Vec<&str> = rpn.split(" ").collect();
+	let mut new: Vec<&str> = vec![];
+	for i in 0..norm.len() {
+		new.push(norm.pop().unwrap());
+	}
+	//store it
+	let mut normal: Vec<&str> = new;
+	let mut stack: Vec<i32> = vec![];
+	//This was a cool idea I had, I will print the rpn string as infix
+	//let mut norm_string = rpntoinfix(normal);
+	let mut value = "";	
+	for i in 0..normal.len() {
+		value = normal.pop().unwrap();
+		if get_precedence(value) == -1 {
+			let num = value.parse::<i32>();
+			stack.push(num.unwrap());
+		}else {
+			let num1 = stack.pop().unwrap();
+			let num2 = stack.pop().unwrap();
+			println!("{} {} {}", num2, value, num1);
+			stack.push(do_op(num2, num1, value));
+		}
+	}
+	println!("Answer: {two}", two = stack.pop().unwrap());
 }
 pub fn help(){
 	println!("MathTools Help: \nFor help; type \"help\"\nFor simple arithmetic(multiplication, addition, subtraction, exponents, etc), type \"simple\"\nFor Reverse polish notation, type \"sausage\"\nTo exit, type \"exit\"");
